1d4533dd5a6cf133f0ce09f72d6075debd404cd7
Jiaying Liu <jiaying_liu1996@hotmail.com>
2020-10-19 20:20:44 -0700

    test

--- server/src/helpers/time.ts
+++ server/src/helpers/time.ts
@@ -7,9 +7,11 @@ export function isMarketHour () {
 	console.log('open hour is', openHour.format('YYYY-MM-DD HH:mm'))
 	const closeHour = moment(`${now.format('YYYY-MM-DD')} 23:59`, 'YYYY-MM-DD HH:mm', 'America/New_York')
 	console.log('close hour is', closeHour.format('YYYY-MM-DD HH:mm'))
-
+	const isBetween = now.isBetween(openHour, closeHour, undefined, '[)')
+	console.log('is between is', isBetween)
 	const value = now.day() >= 1 && now.day() <= 5 && now.isBetween(openHour, closeHour, undefined, '[)')
-
+	console.log('now day is', now.day())
+	console.log('now')
 	console.log('value is', value)
 
 	return value
