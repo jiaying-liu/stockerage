840583805c66d9fc4bc7ee1ebd2936adaa236be3
Jiaying Liu <jiaying_liu1996@hotmail.com>
2020-10-19 20:08:18 -0700

    add timezone

--- server/src/helpers/time.ts
+++ server/src/helpers/time.ts
@@ -3,10 +3,14 @@ import moment = require('moment-timezone')
 export function isMarketHour () {
 	const now = moment().tz('America/New_York')
 	console.log('now is ', now.format('YYYY-MM-DD HH:mm'))
-	const openHour = moment(`${now.format('YYYY-MM-DD')} 09:30`, 'YYYY-MM-DD HH:mm')
+	const openHour = moment(`${now.format('YYYY-MM-DD')} 09:30`, 'YYYY-MM-DD HH:mm', 'America/New_York')
 	console.log('open hour is', openHour.format('YYYY-MM-DD HH:mm'))
-	const closeHour = moment(`${now.format('YYYY-MM-DD')} 16:00`, 'YYYY-MM-DD HH:mm')
+	const closeHour = moment(`${now.format('YYYY-MM-DD')} 16:00`, 'YYYY-MM-DD HH:mm', 'America/New_York')
 	console.log('close hour is', closeHour.format('YYYY-MM-DD HH:mm'))
 
-	return now.day() >= 1 && now.day() <= 5 && now.isBetween(openHour, closeHour, undefined, '[)')
+	const value = now.day() >= 1 && now.day() <= 5 && now.isBetween(openHour, closeHour, undefined, '[)')
+
+	console.log('value is', value)
+
+	return value
 }
